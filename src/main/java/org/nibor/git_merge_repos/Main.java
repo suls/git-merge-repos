2812c3c89bb84c60ed30f4bf2acae808903eb32f
Mathias Sulser <suls@suls.org>
2015-10-15 17:41:06 +0900

    added functionality to set passphrase for ssh keys

--- src/main/java/org/nibor/git_merge_repos/Main.java
+++ src/main/java/org/nibor/git_merge_repos/Main.java
@@ -9,20 +9,49 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UserInfo;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.errors.UnsupportedCredentialItem;
+import org.eclipse.jgit.transport.*;
 
 /**
  * Main class for merging repositories via command-line.
  */
 public class Main {
 
+	public static class Arguments {
+		@Parameter(names = "-pass", password = true, description = "SSH key passphrase")
+		private String passphrase;
+
+		@Parameter(names = "--help", help = true, description = "Displaying command line usage")
+		private boolean help;
+
+		@Parameter(description = "<repository_url>:<target_directory>...")
+		private List<String> repositories = new ArrayList<>();
+	}
+
 	private static Pattern REPO_AND_DIR = Pattern.compile("(.*):([^:]+)");
 
 	public static void main(String[] args) throws IOException, GitAPIException, URISyntaxException {
+
+		final Arguments arguments= new Arguments();
+		JCommander commander = new JCommander(arguments, args);
+
+		if(arguments.help) {
+			commander.usage();
+			return;
+		}
+
+		if(!arguments.passphrase.isEmpty()) {
+			enableSshPassphrase(arguments);
+		}
+
 		List<SubtreeConfig> subtreeConfigs = new ArrayList<>();
 
-		for (String arg : args) {
+		for (String arg : arguments.repositories) {
 			Matcher matcher = REPO_AND_DIR.matcher(arg);
 			if (matcher.matches()) {
 				String repositoryUrl = matcher.group(1);
@@ -35,25 +64,55 @@ public class Main {
 			}
 		}
 
-		if (subtreeConfigs.isEmpty()) {
+		if (!subtreeConfigs.isEmpty()) {
 			exitInvalidUsage("usage: program <repository_url>:<target_directory>...");
+
+
+			File outputDirectory = new File("merged-repo");
+			String outputPath = outputDirectory.getAbsolutePath();
+			System.out.println("Started merging " + subtreeConfigs.size()
+					+ " repositories into one, output directory: " + outputPath);
+
+			long start = System.currentTimeMillis();
+			RepoMerger merger = new RepoMerger(outputPath, subtreeConfigs);
+			List<MergedRef> mergedRefs = merger.run();
+			long end = System.currentTimeMillis();
+
+			long timeMs = (end - start);
+			printIncompleteRefs(mergedRefs);
+			System.out.println("Done, took " + timeMs + " ms");
+			System.out.println("Merged repository: " + outputPath);
 		}
+	}
 
-		File outputDirectory = new File("merged-repo");
-		String outputPath = outputDirectory.getAbsolutePath();
-		System.out.println("Started merging " + subtreeConfigs.size()
-				+ " repositories into one, output directory: " + outputPath);
+	private static void enableSshPassphrase(final Arguments arguments) {
+		JschConfigSessionFactory sessionFactory = new JschConfigSessionFactory() {
+			@Override
+			protected void configure(OpenSshConfig.Host host, Session session) {
+				CredentialsProvider provider = new CredentialsProvider() {
+					@Override
+					public boolean isInteractive() {
+						return false;
+					}
 
-		long start = System.currentTimeMillis();
-		RepoMerger merger = new RepoMerger(outputPath, subtreeConfigs);
-		List<MergedRef> mergedRefs = merger.run();
-		long end = System.currentTimeMillis();
+					@Override
+					public boolean supports(CredentialItem... items) {
+						return true;
+					}
 
-		long timeMs = (end - start);
-		printIncompleteRefs(mergedRefs);
-		System.out.println("Done, took " + timeMs + " ms");
-		System.out.println("Merged repository: " + outputPath);
-
+					@Override
+					public boolean get(URIish uri, CredentialItem... items) throws UnsupportedCredentialItem {
+						for (CredentialItem item : items) {
+							((CredentialItem.StringType) item).setValue(arguments.passphrase);
+						}
+						return true;
+					}
+				};
+				UserInfo userInfo = new CredentialsProviderUserInfo(session, provider);
+				session.setUserInfo(userInfo);
+			}
+		};
+		SshSessionFactory.setInstance(sessionFactory);
 	}
 
 	private static void printIncompleteRefs(List<MergedRef> mergedRefs) {
